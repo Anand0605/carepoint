e6bb1ff7b3311da88962cc93bd9b1de7ebf465cb
Anand Kumar Gautam <anandvns0510@gmail.com>
2025-01-10 23:31:29 +0530

    update

---
+++ src/components/NewsItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NewsItem = () => {
+  return (
+    <div>
+      <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita perspiciatis nihil commodi voluptatum inventore impedit incidunt ipsam minima necessitatibus officiis?
+        
+      </h2>
+    </div>
+  )
+}
+
+export default NewsItem
