482c2ef098a6da9f7a60b750289f7d55efffbf4a
Anand Kumar Gautam <anandvns0510@gmail.com>
2025-01-12 23:24:24 +0530

    update

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -4,6 +4,7 @@ const NewsItem = () => {
   return (
     <div>
       <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita perspiciatis nihil commodi voluptatum inventore impedit incidunt ipsam minima necessitatibus officiis?
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita perspiciatis nihil commodi voluptatum inventore impedit incidunt ipsam minima necessitatibus officiis?
         
       </h2>
     </div>
